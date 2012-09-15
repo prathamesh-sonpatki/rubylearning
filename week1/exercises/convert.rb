97cb987454c1917180390f397b906ea64c0f39fc
Prathamesh Sonpatki <prathamesh.sonpatki@betterlabs.net>
2012-09-15 13:33:20 +0530

    Using proper method name to convey its purpose of converting celcius to farenheit

--- week1/exercises/convert.rb
+++ week1/exercises/convert.rb
@@ -1,12 +1,16 @@
 # week 1 : excercise 6 : convert farenheit to celcius
 
 def convert(temp_f)
-  (Float(temp_f) - 32) * 5 / 9                                     # Using Float(temp_f) to avoid using it in each call of convert method
+  celcius_to_farenheit temp_f
+end
+
+def celcius_to_farenheit(temp_f)
+  (Float(temp_f) - 32) * 5 / 9                                     # Using Float(temp_f) to avoid using it in each call of celcius_to_farenheit method
 end
 
 temp_f = gets
 
 # Kernel#Float checks for the number is float or not, Raises ArgumentError if not
-# Kernel#Float ensures that we don't pass 0.0 to convert method like .to_f does
+# Kernel#Float ensures that we don't pass 0.0 to celcius_to_farenheit method like .to_f does
 
-puts "Temparature in celcius is %.2f " % convert(temp_f)           # Removed Float(temp_f).Used in the convert method itself.
+puts "Temparature in celcius is %.2f " % convert(temp_f)           # Removed Float(temp_f).Used in the celcius_to_farenheit method itself.
