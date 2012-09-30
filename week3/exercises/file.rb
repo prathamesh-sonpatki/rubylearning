fd152670b8b93a226979d5bea7ea41a54f070c93
Prathamesh Sonpatki <prathamesh.sonpatki@betterlabs.net>
2012-09-30 18:21:49 +0530

    Added description for the problem

--- week3/exercises/file.rb
+++ week3/exercises/file.rb
@@ -1,1 +1,23 @@
+# week 3 : exercise 2 : insert word
+
+=begin
+
+test test test test test
+test test test test test
+test test test test test
+test test test test test
+test test test test test
+test test word test test
+test test test test test
+test test test test test
+test test test test test
+test test test test test
+test test test test test
+
+1)Replace 'word' with 'inserted word'
+2)Don't hardcode file name. (ARGV[0])
+3)Use of IO makes it one-liner
+
+=end
+
 IO.write(ARGV[0],IO.readlines(ARGV[0]).map{|line| line.gsub('word', 'inserted word')}.join)
